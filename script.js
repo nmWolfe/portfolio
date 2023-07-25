ab74e684cd8ec481154140fcedfdd7b19b1ec5f0
Nick <n.m.phelan@gmail.com>
2023-07-25 08:50:44 +0100

    Added second function to allow switching between code view and image view in portfolio - yet to implememt

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let formDisplay = document.getElementById("formDisplay");
+// This will grab an elements id, and assign it to the formDisplay variable
+// The function will then toggle the show class on or off
+// Allowing the dropdown menu to display
 
-function toggleForm() {
-  formDisplay.classList.toggle("open-menu");
-}
+const toggleDropdown = (id_name) => {
+  let formDisplay = document.getElementById(id_name);
+  formDisplay.classList.toggle("show");
+};
+
+// Create a switch function that will change the display
+// of the portfolio view to code, or image.
+// Will possibly have to go deeper to switch language
+
+const toggleDisplay = (projectName) => {
+  let currentDisplay = document.getElementById(projectName);
+  currentDisplay.classList.toggle("switch");
+};
